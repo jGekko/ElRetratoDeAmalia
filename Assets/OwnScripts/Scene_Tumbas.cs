5fcf59b56b189658cd1f48593181e4a878146711
Leo13Jocker93 <leoroot13@gmail.com>
2024-10-24 23:24:13 -0500

    ReAjustePrueba
    This reverts commit 158b102fa26fe8e9bc33bc20e912f653f9bbab83.

--- Assets/OwnScripts/Scene_Tumbas.cs
+++ Assets/OwnScripts/Scene_Tumbas.cs
@@ -8,13 +8,11 @@ public class SceneChanger : MonoBehaviour
     public string Tumba_1; // Nombre de la escena a la que quieres cambiar
 
     // Este método se ejecuta cuando ocurre una colisión
-    private void OnTriggerEnter(Collider other)
+    public void OnPointerClickXR()
     {
-        // Verificar si el objeto que colisiona tiene la etiqueta "Player" (puedes ajustarlo según tu caso)
-        if (other.CompareTag("Player"))
-        {
-            // Cambiar a la escena especificada
-            SceneManager.LoadScene(Tumba_1);
-        }
+       
+        // Cambiar a la escena especificada
+        SceneManager.LoadScene(Tumba_1);
+       
     }
 }
